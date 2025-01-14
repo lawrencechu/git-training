86ff9ee9c56af2d09834dd2874341132bb25308e
Lawrence <lawrence.chu@kipptexas.org>
2025-01-14 10:32:28 -0600

    Initial commit: Add SQL files

---
+++ git-training.sql
@@ -0,0 +1,11 @@
+/*****************************************
+
+This is a test .sql file for Git training
+
+*****************************************/
+
+SELECT
+	*
+FROM Consolidated_Enrollment_Historic 
+WHERE
+	Academic_Year = 2024
