ccdf0bc873cf8aac6426f5352717710c04eb7cad
Isaac Wang <isaac.wang@kipptexas.org>
2025-01-28 10:44:55 -0800

    Select student name, school, region, grade & enrollment/withdrawal dates, then filter out No Show students

--- git-training.sql
+++ git-training.sql
@@ -4,8 +4,18 @@ This is a test .sql file for Git training
 
 *****************************************/
 
+-- IW Edits
 SELECT
-	*
-FROM Consolidated_Enrollment_Historic 
+	CONCAT(Name_First, ' ', Name_Last) AS [Student Name]
+	, Student_ID_Local
+	, School_ID
+	, School_Name
+	, KTX_District
+	, Grade
+	, Date_Enrolled
+	, Date_Withdrawn
+	, Withdrawal_Reason
+FROM Consolidated_Enrollment_Historic
 WHERE
 	Academic_Year = 2024
+	AND Withdrawal_Reason <> 'NS'
